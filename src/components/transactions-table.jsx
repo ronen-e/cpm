197f7a8e2c5adf8bdc78d33680038e67665db8f0
ronen-e <alter.ronen@gmail.com>
2016-09-19 15:05:04 +0300

    👥 Add sort ASC ⬆️ and DESC ⬇️

--- src/components/transactions-table.jsx
+++ src/components/transactions-table.jsx
@@ -3,6 +3,9 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import { Transaction } from '../models/transaction';
 import { Style } from 'radium';
 
+const ASC = 'ASC';
+const DESC = 'DESC';
+
 export default class TransactionsTable extends Component {
     static displayName = 'TransactionsTable';
     static propTypes = {
@@ -12,20 +15,46 @@ export default class TransactionsTable extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { sortKey: 'id' };
+        this.state = { sortKey: 'id', direction: ASC };
     }
-    sortBy(sortKey) {
-        this.setState({ sortKey });
+    sortBy(sortKey, direction = ASC) {
+        if (sortKey === this.state.sortKey && this.state.direction === ASC) {
+            direction = DESC;
+        }
+        this.setState({ sortKey, direction });
+    }
+
+    comparator(a, b, direction) {
+        var result;
+        switch (typeof a) {
+            case 'number':
+                result = a - b;
+                break;
+            case 'string':
+                result = a.localeCompare(b);
+                break;
+        }
+
+        if (direction === DESC) {
+            result = -(result);
+        }
+        return result;
     }
 
     render() {
         var { items } = this.props;
+        var { sortKey, direction } = this.state;
+
         var tableStyle = (
-            <Style scopeSelector='.transactions-table' rules={{
+            <Style scopeSelector=".transactions-table" rules={{
                     th: { fontWeight: 'normal' }
                 }} />
         );
-        items = items.sortBy((item) => item[this.state.sortBy]);    // TODO - cache results if key was not changed
+
+        items = items.sortBy(
+            (item) => item[sortKey],
+            (a, b) => this.comparator(a, b, direction)
+        );
 
         return (
             <div className="table-responsive transactions-table">
@@ -33,11 +62,11 @@ export default class TransactionsTable extends Component {
                 <table className="table table-condensed table-striped">
                     <thead>
                         <tr>
-                            <th onClick={() => this.sortBy('id')}>ID</th>
-                            <th onClick={() => this.sortBy('purchaseDate')}>DATE</th>
-                            <th onClick={() => this.sortBy('itemId')}>Item ID</th>
-                            <th onClick={() => this.sortBy('name')}>Item Name</th>
-                            <th onClick={() => this.sortBy('price')}>Payment ($)</th>
+                            <th onClick={ () => this.sortBy('id') }>ID</th>
+                            <th onClick={ () => this.sortBy('purchaseDate') }>DATE</th>
+                            <th onClick={ () => this.sortBy('itemId') }>Item ID</th>
+                            <th onClick={ () => this.sortBy('name') }>Item Name</th>
+                            <th onClick={ () => this.sortBy('price') }>Payment ($)</th>
                         </tr>
                     </thead>
                     <tbody>
